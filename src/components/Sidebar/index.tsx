f055056cf6608aed9e2b838ce990d2fba203e4ee
FeAranha <felipearanha.c@gmail.com>
2024-02-29 17:35:34 -0300

    Menu de Navegação

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,20 @@
 import { Search } from "lucide-react";
 import { Logo } from "./Logo";
+import { MainNavigation } from "./MainNavigation";
 
 export function Sidebar() {
   return(
-    <aside className="border-r border-zinc-300 dark:border-zinc-200 px-5 py-8 space-y-6">
+    <aside className="border-r border-zinc-300 dark:border-violet-950 px-5 py-8 space-y-6">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-3">
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-3">
         <Search className="h-5 w-5 to-zinc-500"/>
         <input 
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-500" 
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-500 dark:text-zinc-300" 
           placeholder="Search" />
       </div>
+
+      <MainNavigation />
     </aside>
   )
 }
