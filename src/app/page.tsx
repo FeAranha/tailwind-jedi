e6e0aec704dc95d1e2b4b219be4662a16ea44ee3
FeAranha <felipearanha.c@gmail.com>
2024-02-29 14:05:25 -0300

    Valores Arbitrários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="dark:bg-slate-900 p-8 bg-slate-100 dark:text-slate-50 text-slate-900 h-screen flex flex-col items-center text-center">
-      <div className="max-w-2xl">
+      <div className="max-w-app">
         <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl">
           Rapidly build modern websites without ever leaving your HTML.
         </h1>
