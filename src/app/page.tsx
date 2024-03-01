079fdcd16808f2a74ba378123e33addbe42bc3e0
FeAranha <felipearanha.c@gmail.com>
2024-03-01 16:11:47 -0300

    Criando Abas com Radix Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Dashboard 🧮</h1>
+  return (
+    <>
+      <h1 className="text-3xl">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
