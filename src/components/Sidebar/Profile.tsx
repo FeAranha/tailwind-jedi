21ac261a6ea2b5dd0af0fac2eebcdf6a54ea321d
FeAranha <felipearanha.c@gmail.com>
2024-03-01 00:44:38 -0300

    Perfil do Usuário

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -3,20 +3,24 @@ import { LogOut } from 'lucide-react'
 export function Profile() {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="https://github.com/fearanha.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
-      <div className="flex flex-col">
-        <span className="text-sm font-semibold text-zinc-700">
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-400">
           Felipe Aranha
         </span>
         <span className="truncate text-sm text-zinc-500">
-          fearanha@email.com
+          suhsahufearanha@email.com
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-50 dark:hover:bg-slate-600"
+      >
         <LogOut className="h-5 w-5 text-zinc-500" />
       </button>
     </div>
