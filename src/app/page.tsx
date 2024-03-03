b17b5ec62faf188c7df097f042d4d4e0d3d42ae3
FeAranha <felipearanha.c@gmail.com>
2024-03-03 11:27:07 -0300

    Upload do Avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { SettingsTabs } from '@/components/SettingsTabs'
 import * as Input from '@/components/Input'
-import { Mail } from 'lucide-react'
+import { Mail, UploadCloud, User } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -40,7 +40,7 @@ export default function Home() {
           id="settings"
           className="mt-6 flex w-full flex-col gap-6 divide-y  divide-zinc-200 dark:divide-zinc-600 dark:text-zinc-400"
         >
-          <div className="grid-cols-form grid gap-3">
+          <div className="grid grid-cols-form gap-3">
             <label
               htmlFor="firstName"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
@@ -58,7 +58,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="email"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
@@ -77,9 +77,9 @@ export default function Home() {
             </Input.Root>
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
-              htmlFor="email"
+              htmlFor="photo"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
             >
               Your photo
@@ -87,10 +87,37 @@ export default function Home() {
                 This will be displayed on your profile.
               </span>
             </label>
-            <div />
+            <div className="flex items-start gap-5">
+              <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+                <User className="h-8 w-8 text-violet-500" />
+              </div>
+
+              <label
+                htmlFor="photo"
+                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500 dark:hover:bg-slate-800"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+
+              <input type="file" className="sr-only" id="photo" />
+            </div>
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="role"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
@@ -102,7 +129,7 @@ export default function Home() {
             </Input.Root>
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="country"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
@@ -112,7 +139,7 @@ export default function Home() {
             <div />
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="timezone"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
@@ -122,7 +149,7 @@ export default function Home() {
             <div />
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="bio"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
@@ -135,7 +162,7 @@ export default function Home() {
             <div />
           </div>
 
-          <div className="grid-cols-form grid gap-3 pt-5">
+          <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="projects"
               className="text-sm font-medium text-zinc-700 dark:text-zinc-400"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,18 @@ module.exports = {
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
 
-      maxWidth: {
-        app: '700px',
+      borderWidth: {
+        6: '6px',
       },
 
       colors: {
-        diego: '#333',
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
+      maxWidth: {
+        app: '700px',
       },
     },
   },
