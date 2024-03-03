f8807cd4ebde686b80b38a85113555c181aec4ea
FeAranha <felipearanha.c@gmail.com>
2024-03-03 18:58:31 -0300

    Criando efeitos de foco

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,7 +15,7 @@ type InputControlProps = ComponentProps<'input'>
 export function Control(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-500 dark:text-zinc-400"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-500 outline-none dark:text-zinc-400"
       {...props}
     />
   )
@@ -26,7 +26,7 @@ export type InputRootProps = ComponentProps<'div'>
 export function Root(props: InputRootProps) {
   return (
     <div
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-3"
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-3 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...props}
     />
   )
